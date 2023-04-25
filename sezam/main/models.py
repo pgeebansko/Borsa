1d32eb235eacec87f415c5f6c7f5003f74d344ae
pgeebansko <pgee_students@abv.bg>
2023-04-25 12:00:53 +0300

    добавени снимки

--- sezam/main/models.py
+++ sezam/main/models.py
@@ -16,3 +16,31 @@ class Klas(models.Model):
         verbose_name = 'Клас'
         verbose_name_plural = 'Класове'
 
+
+# списък предложения
+class Offer(models.Model):
+    title = models.CharField('Име', max_length=100, default='', help_text='име на офертата')
+    class_num = models.ForeignKey(Klas, name='class_id', on_delete=models.SET_NULL, null=True, blank=True)
+    content = models.CharField('съдържание', max_length=100, default='')
+    price = models.CharField('цена', max_length=50, default='')
+    remark = models.CharField('коментар', max_length=100, default='')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Предложение'
+        verbose_name_plural = 'Предложения'
+
+
+# списък предложения
+class Comment(models.Model):
+    text = models.CharField('Име', max_length=100, default='')
+    offer_id = models.ForeignKey(Offer, on_delete=models.SET_NULL, null=True, blank=True)
+
+    def __str__(self):
+        return self.text
+
+    class Meta:
+        verbose_name = 'Предложение'
+        verbose_name_plural = 'Предложения'
